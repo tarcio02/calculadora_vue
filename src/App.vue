47bdcc70006b4aec0130d506ac34a63bf6f21543
tarcio teixeia <tarciogbteixeira@gmail.com>
2024-01-10 19:11:38 -0200

    criando layout & iniciando parte logica

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,45 @@
 <script setup>
 import { reactive } from 'vue';
+import operation from './components/operation.vue'
+import buttons from './components/buttons.vue'
 
-const previusOperationText = document.querySelector("#previous-operation");
-const currentOperationText = document.querySelector("#current-operation");
-const buttons = document.querySelectorAll("#buttons-container button");
-
-class Calculator {
-
-} 
-
-buttons.forEach((btn) => {
-
-  btn.addEventListener("click", (e) => {
-    const value = e.target.innerText;
-
-    console.log(value);
-  });
-
+const estado = reactive ({
+  operator: '',
+  num1:'',
+  num2: ''
 });
+
+const resultado = ()=> {
+  const {operator} = estado
+  switch(operator) {
+    case'-':
+    return parseFloat(estado.num1) - parseFloat(estado.num2)
+    case'*':
+    return parseFloat(estado.num1) * parseFloat(estado.num2)
+    case'/':
+    return parseFloat(estado.num1) / parseFloat(estado.num2)
+    case'+':
+    return  parseFloat(estado.num1) + parseFloat(estado.num2)
+  }
+}
 
 
 </script>
 
 <template>
 <body>
+
   <div id="calc">
-  <h3>Calculadora</h3>
-  <div id="operations">
-    <div id="previus-operation"></div>
-    <div id="current-operation"></div>
-  </div>
-  <div id="buttons-container">
-    <button>CE</button>
-    <button>C</button>
-    <button>DEL</button>
-    <button>/</button>
-    <button class="number">7</button>
-    <button class="number">8</button>
-    <button class="number">9</button>
-    <button>*</button>
-    <button class="number">4</button>
-    <button class="number">5</button>
-    <button class="number">6</button>
-    <button>-</button>
-    <button class="number">1</button>
-    <button class="number">2</button>
-    <button class="number">3</button>
-    <button>+</button>
-    <button class="number">0</button>
-    <button class="number">.</button>
-    <button id="equal-btn">=</button>
-  </div>
+  <h3>Calculadora vue.js</h3>
+  <operation/>
+  <buttons/>
 </div>
+
 </body>
 </template>
 
 <style scoped>
+
 * {
   margin: 0;
   padding: 0;
@@ -86,56 +70,9 @@ body {
   color: #666;
 }
 
-#operations {
-  text-align: right;
-}
+/* operations */
 
-#current-operation,
-#previus-operation {
-  min-height: 1.6em;
-  padding: 0.2em;
-  overflow-wrap: break-word;
-}
+/* buttons */
 
-#previus-operation {
-  color: #777;
-}
 
-#current-operation {
-  font-size: 3em;
-  font-weight: 700;
-}
-
-#buttons-container {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  gap: 3px;
-  }
-
-  #buttons-container button {
-    border: 1px solid transparent;
-    border-radius: 0;
-    height: 4em;
-    font-size: 1.2em;
-    background-color: #dbdbdb;
-    cursor: pointer;
-  }
-
-  #buttons-container .number {
-    background-color: #f3f3f3;
-  }
-
-  #buttons-container button:hover {
-    background-color: #bababa;
-    border-color: #999;
-  }
-
-  #buttons-container #equal-btn {
-    grid-column: span 2;
-    background-color: #75a5cb;
-  }
-
-  #buttons-container #equal-btn:hover {
-    background-color: #3a91d8;
-  }
 </style>
---
+++ src/components/operation.vue
@@ -0,0 +1,46 @@
+<script setup>
+    
+</script>
+
+<template>
+
+<div id="operations">
+    <div class="nums">
+        <div id="num1"> 15</div>
+        <div id="num2"> +56</div>
+    </div>
+    <div id="result"> </div>
+</div>
+
+</template>
+
+<style scoped>
+
+#operations {
+    text-align: right;
+}
+
+.nums {
+    display: flex;
+}
+
+#result,
+#num1,
+#num2{
+    min-height: 1.6em;
+    padding: 0.2em;
+    overflow-wrap: break-word;
+}
+
+#num1,
+#num2{
+    color: #777;
+    max-width: 50%;
+    width: 100%;
+}
+
+#result {
+    font-size: 3em;
+    font-weight: 700;
+}
+</style>
